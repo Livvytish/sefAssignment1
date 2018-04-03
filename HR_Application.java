2a19029e4411bbbba03ac0e49bc5cde90c31fa05
StevenRmitIT <30486069+StevenRmitIT@users.noreply.github.com>
2018-04-04 00:14:16 +1000

    Add files via upload

--- HR_Application.java
+++ HR_Application.java
@@ -1,13 +1,41 @@
 package Application;
 
+import Module.Request;
+
 public class HR_Application {
 	
 	public boolean login(String username, String password)
 	{
 		return true;
 	}
-	public void runningSystem()
+	
+	public void viewRequests()
 	{
 		
+	}
+	
+	public boolean submitRequest(int reqIndex)
+	{
+		return false;
+	}
+	
+	public void createRequest()
+	{
+		
+	}
+	
+	public boolean editRequest(int reqIndex)
+	{
+		return false;
+	}
+	
+	public boolean permitRequest(int reqIndex)
+	{
+		return false;
+	}
+	
+	public boolean denyRequest(int reqIndex)
+	{
+		return false;
 	}
 }
